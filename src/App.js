76991d99377cfc9212eb81ad06ef7bd2a4bee59f
mariata06 <slinkywizard@mail.ru>
2023-03-12 21:32:50 +0300

    Add expenses component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,54 @@
 import React from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses';
 
 function App() {
   const expenses = [
     {
+      id: 'e1',
       title: "Car Insurance",
       amount: 250,
       date: new Date(2023, 2, 12),
     },
     {
-      title: "gas",
+      id: 'e2',
+      title: "Gas",
       amount: 50,
       date: new Date(2022, 1, 10),
     },
     {
-      title: "electricity",
+      id: 'e3',
+      title: "Electricity",
       amount: 100,
       date: new Date(2020, 5, 20),
     },
     {
-      title: "food",
+      id: 'e4',
+      title: "Food",
       amount: 200,
       date: new Date(2022, 3, 7),
     },
-    {
-      title: "react course",
+    { 
+      id: 'e5',
+      title: "React Course",
       amount: 1000,
       date: new Date(2020, 4, 15),
     },
     {
+      id: 'e6',
       title: "Books",
       amount: 200,
       date: new Date(2023, 2, 22),
     },
     {
-      title: "booking",
+      id: 'e7',
+      title: "Booking",
       amount: 500,
       date: new Date(2021, 6, 25),
     },
   ]
   return (
     <div className="App">     
-      <h2>Hello React!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date } />
+      <Expenses items={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,12 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+    return (
+        <div className='expenses'>
+            {props.items.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />)}
+        </div>
+    )
+}
+
+export default Expenses;

77a2d9a5f7f63c2d9b886e063a5d2f2298e2ec05
mariata06 <slinkywizard@mail.ru>
2023-03-12 19:56:06 +0300

    Add expense item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 250,
+      date: new Date(2023, 2, 12),
+    },
+    {
+      title: "gas",
+      amount: 50,
+      date: new Date(2022, 1, 10),
+    },
+    {
+      title: "electricity",
+      amount: 100,
+      date: new Date(2020, 5, 20),
+    },
+    {
+      title: "food",
+      amount: 200,
+      date: new Date(2022, 3, 7),
+    },
+    {
+      title: "react course",
+      amount: 1000,
+      date: new Date(2020, 4, 15),
+    },
+    {
+      title: "Books",
+      amount: 200,
+      date: new Date(2023, 2, 22),
+    },
+    {
+      title: "booking",
+      amount: 500,
+      date: new Date(2021, 6, 25),
+    },
+  ]
   return (
     <div className="App">     
-      Hello React!
+      <h2>Hello React!</h2>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date } />
     </div>
   );
 }
